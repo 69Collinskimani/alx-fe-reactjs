de3151072af7e9dc21c84308d6edfb298b446007
69Collinskimani <fackerjames40@gmail.com>
2024-11-02 15:37:22 +0300

    Modify JSX in a Pre-built React Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,5 @@
+import WelcomeMessage from './components/WelcomeMessage';
+<WelcomeMessage></WelcomeMessage>
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -28,6 +30,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+            <WelcomeMessage />
+            {/* Other components or elements can go here */}
+        </div>
     </>
   )
 }
