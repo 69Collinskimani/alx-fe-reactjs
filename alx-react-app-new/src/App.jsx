f0388eb1c19dffa2dadb0c9f92fd2053de2f8063
69Collinskimani <fackerjames40@gmail.com>
2024-11-12 22:06:50 +0300

    adding counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 
@@ -47,6 +48,11 @@ function App() {
             <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
             <UserProfile name="Bob" age="30" bio="Avid traveler and foodie" />
         </div>
+        <div>
+          <hi>Counter Application</hi>
+          <Counter/>
+        </div>
+        
 
     </>
   )
