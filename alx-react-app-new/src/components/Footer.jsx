f0e5ee1b742d04bc8f935a6dc64368e8ee5f0d11
69Collinskimani <fackerjames40@gmail.com>
2024-11-12 18:50:06 +0300

    react

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer>
+             <footer style={{ backgroundColor: 'darkgray', color: 'white', textAlign: 'center', padding: '10px' }}></footer>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}></header>
+            <h1>My Favorite Cities</h1>
+        </header>
+    );
+}
+
+export default Header;
