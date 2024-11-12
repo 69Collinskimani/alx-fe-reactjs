7eaab814962f5cbce221917224151dc8d1ada69e
69Collinskimani <fackerjames40@gmail.com>
2024-11-12 18:53:31 +0300

    correcting maincontent

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,8 @@ function MainContent() {
     return (
         <main style={{ padding: '20px', backgroundColor: '#f4f4f4', marginTop: '20px' }}>
         <h2 style={{ color: 'green' }}>Welcome to the main content section!</h2>
-        <p>Here, we explore different topics about cities around the world.</p>
+        <p>I love to visit New York, Paris, and Tokyo</p>
+
       </main>
     );
 }
