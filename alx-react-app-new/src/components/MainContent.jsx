3b53b8d68c5dd3f472b4ed9f63d311060cbef4c0
69Collinskimani <fackerjames40@gmail.com>
2024-11-12 18:55:50 +0300

    react

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,9 +1,9 @@
 function MainContent() {
     return (
         <main style={{ padding: '20px', backgroundColor: '#f4f4f4', marginTop: '20px' }}>
-        <h2 style={{ color: 'green' }}>Welcome to the main content section!</h2>
-        <p>I love to visit New York, Paris, and Tokyo</p>
-
+        <h2 style={{ color: 'green' }}>I love to visit New York, Paris, and Tokyo</h2>
+        <p>Here, we explore different topics about cities around the world.</p>
+        
       </main>
     );
 }
